4fa6dcfa71ce09daa8c6c9bfe8ecc2a4f1b71af6
Sandelier <miika.saily@gmail.com>
2024-07-07 08:15:26 +0300

    For some reason i had duplicated the intercepting fetch which was causing an issue.

--- firefox/contentScripts/helper-interceptFetch.js
+++ firefox/contentScripts/helper-interceptFetch.js
@@ -45,31 +45,3 @@
     
 	document.head.appendChild(script);
 })();
-
-
-const originalFetch = window.fetch;
-window.fetch = async (...args) => {
-    try {
-        const response = await originalFetch(...args);
-    
-        if (args[0].startsWith('/api/custom_lists/applicable/manga/') && response.status === 200) {
-            const clonedResponse = response.clone();
-        
-            clonedResponse.text().then(body => {
-                window.postMessage({
-                    action: "userCustomLists",
-                    body: body,
-                    entryInfo: window.AP_VARS.ENTRY_INFO,
-                }, "*");
-            
-            }).catch(error => {
-                console.error('Error reading response body:', error);
-            });
-        }
-    
-        return response;
-    } catch (error) {
-        console.error('Fetch error:', error);
-        throw error;
-    }
-};
